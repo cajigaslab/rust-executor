4556973a9e8d4456ca1de3ec0778ed227f5d3309
Jarl Haggerty <jarlhaggerty@gmail.com>
2026-08-12 10:44:32 -0400

    Fix get_color

--- src/behavior_task/config_util.rs
+++ src/behavior_task/config_util.rs
@@ -8,9 +8,9 @@ use itertools;
 use rand::seq::{IndexedRandom};
 
 pub fn get_color(value: &serde_json::Value) -> Color4f {
-  let r = value.as_f64().unwrap()/255.0;
-  let g = value.as_f64().unwrap()/255.0;
-  let b = value.as_f64().unwrap()/255.0;
+  let r = value[0].as_f64().unwrap()/255.0;
+  let g = value[1].as_f64().unwrap()/255.0;
+  let b = value[2].as_f64().unwrap()/255.0;
   Color4f::new(r as f32, g as f32, b as f32, 1.0)
 }
 
